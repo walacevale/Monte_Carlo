bbca3dd699f1cd7e2a823150c3f10603b80b1388
Wallace Vale <wallacevale123@gmail.com>
2025-01-13 17:31:26 -0300

    criacao da funcao histograma

---
+++ Lista 1/tools.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+
+def histograma(dados, num_bins=50, normalizar=False):
+    """
+    Calcula o histograma de uma amostra de dados e retorna as frequências e os limites dos intervalos (bins).
+
+    Parâmetros:
+    ----------
+    dados : array
+        Dados de entrada para a criação do histograma.
+    num_bins : int, opcional
+        Número de intervalos (bins) para o histograma. Padrão é 50.
+    normalizar : bool, opcional
+        Se True, normaliza o histograma para representar uma densidade de probabilidade. Padrão é False.
+
+    Retorna:
+    -------
+    frequencias : list
+        Frequências de cada intervalo (bin).
+    limites_bins : numpy.ndarray
+        Limites dos intervalos (bins).
+    """
+    dados = np.array(dados)
+    
+    dados_ordenados = np.sort(dados)
+    valor_maximo, valor_minimo = dados_ordenados.max(), dados_ordenados.min()
+    largura_bin = (valor_maximo - valor_minimo) / num_bins
+
+    frequencias = []
+
+    # Calculando as frequências para cada intervalo (bin)
+    for i in range(num_bins):
+        limite_inferior = valor_minimo + i * largura_bin
+        limite_superior = valor_minimo + (i + 1) * largura_bin
+
+        # Selecionando os valores dentro do intervalo
+        valores_no_bin = np.where((limite_inferior <= dados_ordenados) & (dados_ordenados < limite_superior), dados_ordenados, 0)
+        frequencia = len(valores_no_bin[valores_no_bin != 0])
+        frequencias.append(frequencia)
+
+    if normalizar:
+        area_total = np.sum(frequencias) * largura_bin
+        frequencias = np.array(frequencias) / area_total
+
+    limites_bins = np.linspace(valor_minimo, valor_maximo, num_bins)
+
+    return frequencias, limites_bins
+
+
+
+def E(x,k):
+    return 0.5*k*x**2
+
+
+def media(x):
+    """
+    Calcula a média de uma lista de números em 1D.
+    
+    Parâmetros:
+    ----------
+    x : lista ou array
+        Dados de entrada para o cálculo da média.
+    
+    Retorna:
+    -------
+    média : float
+        Média dos valores de x.
+    """
+    return sum(x)/len(x)
